a60be79d3d09239a75ee8f3b9146d8d3fe8d5ccd
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-05-10 16:52:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -64,13 +64,10 @@ dotenv.config();  // Load environment variables
 
 const app = express();
 // app.use(cors());
-// app.use(cors({
-//   origin: 'https://fitnesstest-4.onrender.com', // ✅ Your frontend URL
-//   credentials: true
-// }));
 app.use(cors({
   origin: 'https://fitnesstest-4.onrender.com', // allow your frontend URL
   credentials: true, // if you're using cookies or HTTP auth
+   methods:["GET","POST","PUT","DELETE"],
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
