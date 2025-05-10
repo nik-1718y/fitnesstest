255bc5362c0956960ee01c5d87bc6f41ce6eaba0
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-05-10 14:42:22 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -22,10 +22,17 @@ export const getDashboardDetails = async (token) =>
 
 
 
-    export const getWorkouts = async (token, date) =>
-        await API.get(`/user/workout${date}`, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
+//10/5/25
+    // export const getWorkouts = async (token, date) =>
+    //     await API.get(`/user/workout${date}`, {
+    //       headers: { Authorization: `Bearer ${token}` },
+    //     });
+
+export const getWorkouts = async (token, date) =>
+  await API.get(`/user/workout?date=${date}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
       
       export const addWorkout = async (token, data) =>
         await API.post(`/user/workout`, data, {
