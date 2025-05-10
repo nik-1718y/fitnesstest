b8b55ec41b873f7083427aadc1ebe730d85af0d7
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-05-11 00:21:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -109,11 +109,11 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     connectDB();
-    // app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(8080, () => console.log("Server started on port 8080"));
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
   } catch (error) {
     console.error("Error starting the server:", error);
