77d5486bfff45b0df3df252cc75aff5acd1b9452
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-05-10 17:40:49 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -217,29 +217,16 @@ const Dashboard = () => {
 -30 kg
 -10 min`);
 
-  // const dashboardData = async () => {
-  //   setLoading(true);
-  //   const token = localStorage.getItem("fitness");
-  //   await getDashboardDetails(token).then((res) => {
-  //     setData(res.data);
-  //     console.log(res.data);
-  //     setLoading(false);
-  //   });
-  // };
   const dashboardData = async () => {
-  setLoading(true);
-  const token = localStorage.getItem("fitness");
-  try {
-    const res = await getDashboardDetails(token);
-    setData(res);
-    console.log(res);
-    setLoading(false);
-  } catch (error) {
-    console.error('Network error:', error.message);  // Add error logging
-    setLoading(false); // Stop loading spinner if there is an error
-    alert('There was an issue fetching the data. Please try again later.');
-  }
-};
+    setLoading(true);
+    const token = localStorage.getItem("fitness");
+    await getDashboardDetails(token).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+      setLoading(false);
+    });
+  };
+
 
   const getTodaysWorkout = async () => {
     setLoading(true);
